7f691c8e858d8d24045fa31b8bfe314d5848485c
pahv <93768750+pahv@users.noreply.github.com>
2021-11-10 21:50:36 +0000

    //

--- Main/app.py
+++ Main/app.py
@@ -1,21 +1,36 @@
 import tkinter as tk
 
-HEIGHT = 700
-WIDTH = 800
-
-root = tk.Tk()
-
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="#FFFFFF")
-canvas.pack()
-
-frame = tk.Frame(root, bg="#000000")
-frame.place(relwidth=1, relheight=1)
-
-label = tk.Label(frame, text="Tkinter Test Program")
-label.pack(fill='x')
+window = tk.Tk()
 
 
-button = tk.Button(frame, text="Test Button", bg='white', fg='black')
-button.pack(fill='x')
+window.title("Tkinter Template")
 
-root.mainloop()
+window.geometry("862x519")
+window.configure(bg="#3A7FF6")
+canvas = tk.Canvas(
+    window, bg="#3A7FF6", height=519, width=862,
+    bd=0, highlightthickness=0, relief="ridge")
+canvas.place(x=0, y=0)
+canvas.create_rectangle(431, 0, 431 + 431, 0 + 519, fill="#FCFCFC", outline="")
+canvas.create_rectangle(40, 160, 40 + 60, 160 + 5, fill="#FCFCFC", outline="")
+
+title = tk.Label(
+    text="Welcome to Tkinter Designer", bg="#3A7FF6",
+    fg="white", font=("Arial-BoldMT", int(20.0)))
+title.place(x=27.0, y=120.0)
+
+info_text = tk.Label(
+    text="This is a program that I have made\n"
+    "to act as a basic template for any\n"
+    "tkinter projects that I create in the future.\n\n\n"
+    "Made by Pahv\n\n",
+    bg="#3A7FF6", fg="white", justify="left",
+    font=("Georgia", int(16.0)))
+
+info_text.place(x=27.0, y=200.0)
+
+
+
+
+window.resizable(False, False)
+window.mainloop()
