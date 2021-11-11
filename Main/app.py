0f7fcec5a147ef95be4fe31b0c324f753f477c75
pahv <93768750+pahv@users.noreply.github.com>
2021-11-11 23:29:06 +0000

    //

--- Main/app.py
+++ Main/app.py
@@ -30,9 +30,7 @@ class SampleApp(tk.Tk):
 
         self.title_font = tkfont.Font(family='Helvetica', size=12)
 
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible 
-        # will be raised above the others
+    
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
@@ -44,9 +42,6 @@ class SampleApp(tk.Tk):
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
 
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame("StartPage")
